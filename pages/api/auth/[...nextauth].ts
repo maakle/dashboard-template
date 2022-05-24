84a7dc81392b4a27d4aa33787b0c6762667685ba
Mathias Klenk <mathiasklenk@gmx.de>
2022-05-24 12:12:56 +0200

    Refactored prisma out and centered loading spinner

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,11 +1,9 @@
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import { PrismaClient } from '@prisma/client';
 import NextAuth from 'next-auth';
 import EmailProvider from 'next-auth/providers/email';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
-
-const prisma = new PrismaClient();
+import prisma from '../../../lib/prisma';
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
