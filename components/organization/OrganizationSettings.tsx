e3f9baa15e744126fc65352f590bd81ee80259ab
Mathias Klenk <mathiasklenk@gmx.de>
2022-05-29 20:40:36 +0200

    Built the organization setting profile

--- components/organization/OrganizationSettings.tsx
+++ components/organization/OrganizationSettings.tsx
@@ -2,15 +2,20 @@ import {
   Box,
   Button,
   chakra,
-  FormControl,
-  FormLabel,
+  Divider,
   GridItem,
   Heading,
   Input,
-  Select,
   SimpleGrid,
   Stack,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
   Text,
+  Th,
+  Thead,
+  Tr,
   useColorModeValue
 } from '@chakra-ui/react';
 
@@ -32,7 +37,7 @@ const OrganizationSettings = () => {
               fontSize="sm"
               color={useColorModeValue('gray.600', 'gray.400')}
             >
-              Use a permanent address where you can receive mail.
+              You can manage your organization's information & team member here.
             </Text>
           </Box>
         </GridItem>
@@ -50,158 +55,68 @@ const OrganizationSettings = () => {
               bg={useColorModeValue('white', 'gray.700')}
               spacing={6}
             >
-              <SimpleGrid columns={6} spacing={6}>
-                <FormControl as={GridItem} colSpan={4}>
-                  <FormLabel
-                    htmlFor="first_name"
-                    fontSize="sm"
-                    fontWeight="md"
-                    color={useColorModeValue('gray.700', 'gray.50')}
-                  >
-                    Company Name
-                  </FormLabel>
-                  <Input
-                    type="text"
-                    name="company_name"
-                    id="company_name"
-                    autoComplete="company"
-                    mt={1}
-                    shadow="sm"
-                    size="sm"
-                    w="full"
-                    rounded="md"
-                  />
-                </FormControl>
+              <Box>
+                <Heading as="h5" size="sm" mb={2}>
+                  Organization Name
+                </Heading>
 
-                <FormControl as={GridItem} colSpan={6}>
-                  <FormLabel
-                    htmlFor="street_address"
-                    fontSize="sm"
-                    fontWeight="md"
-                    color={useColorModeValue('gray.700', 'gray.50')}
-                  >
-                    Street address
-                  </FormLabel>
-                  <Input
-                    type="text"
-                    name="street_address"
-                    id="street_address"
-                    autoComplete="street-address"
-                    mt={1}
-                    shadow="sm"
-                    size="sm"
-                    w="full"
-                    rounded="md"
-                  />
-                </FormControl>
+                <Input
+                  type="text"
+                  name="company_name"
+                  id="company_name"
+                  autoComplete="company"
+                  mt={1}
+                  shadow="sm"
+                  size="sm"
+                  w="full"
+                  rounded="md"
+                  maxWidth={400}
+                />
 
-                <FormControl as={GridItem} colSpan={[6, 6, null, 2]}>
-                  <FormLabel
-                    htmlFor="city"
-                    fontSize="sm"
-                    fontWeight="md"
-                    color={useColorModeValue('gray.700', 'gray.50')}
-                  >
-                    City
-                  </FormLabel>
-                  <Input
-                    type="text"
-                    name="city"
-                    id="city"
-                    autoComplete="city"
-                    mt={1}
-                    shadow="sm"
-                    size="sm"
-                    w="full"
-                    rounded="md"
-                  />
-                </FormControl>
+                <Divider marginY={10} />
 
-                <FormControl as={GridItem} colSpan={[6, 3, null, 2]}>
-                  <FormLabel
-                    htmlFor="state"
-                    fontSize="sm"
-                    fontWeight="md"
-                    color={useColorModeValue('gray.700', 'gray.50')}
-                  >
-                    State / Province
-                  </FormLabel>
-                  <Input
-                    type="text"
-                    name="state"
-                    id="state"
-                    autoComplete="state"
-                    mt={1}
-                    shadow="sm"
-                    size="sm"
-                    w="full"
-                    rounded="md"
-                  />
-                </FormControl>
+                <Heading as="h5" size="sm" mb={2}>
+                  Team
+                </Heading>
 
-                <FormControl as={GridItem} colSpan={[6, 3, null, 2]}>
-                  <FormLabel
-                    htmlFor="postal_code"
-                    fontSize="sm"
-                    fontWeight="md"
-                    color={useColorModeValue('gray.700', 'gray.50')}
-                  >
-                    ZIP / Postal
-                  </FormLabel>
-                  <Input
-                    type="text"
-                    name="postal_code"
-                    id="postal_code"
-                    autoComplete="postal-code"
-                    mt={1}
-                    shadow="sm"
-                    size="sm"
-                    w="full"
-                    rounded="md"
-                  />
-                </FormControl>
-
-                <FormControl as={GridItem} colSpan={3}>
-                  <FormLabel
-                    htmlFor="country"
-                    fontSize="sm"
-                    fontWeight="md"
-                    color={useColorModeValue('gray.700', 'gray.50')}
-                  >
-                    Country / Region
-                  </FormLabel>
-                  <Select
-                    id="country"
-                    name="country"
-                    autoComplete="country"
-                    placeholder="Select option"
-                    mt={1}
-                    shadow="sm"
-                    size="sm"
-                    w="full"
-                    rounded="md"
-                  >
-                    <option>United States</option>
-                    <option>Canada</option>
-                    <option>Mexico</option>
-                  </Select>
-                </FormControl>
-              </SimpleGrid>
+                <TableContainer>
+                  <Table variant="simple">
+                    <Thead>
+                      <Tr>
+                        <Th>Name</Th>
+                        <Th>Role</Th>
+                        <Th>Action</Th>
+                      </Tr>
+                    </Thead>
+                    <Tbody>
+                      <Tr>
+                        <Td>Mathias</Td>
+                        <Td>Admin</Td>
+                        <Td>
+                          <Button colorScheme="red" size="xs">
+                            Remove
+                          </Button>
+                        </Td>
+                      </Tr>
+                    </Tbody>
+                  </Table>
+                </TableContainer>
+              </Box>
+              <Box px={{ base: 4, sm: 6 }} py={3} textAlign="right">
+                <Button
+                  backgroundColor="gray.900"
+                  color="white"
+                  fontWeight="medium"
+                  _hover={{ bg: 'gray.700' }}
+                  _active={{
+                    bg: 'gray.800',
+                    transform: 'scale(0.95)'
+                  }}
+                >
+                  Save
+                </Button>
+              </Box>
             </Stack>
-            <Box px={{ base: 4, sm: 6 }} py={3} textAlign="right">
-              <Button
-                backgroundColor="gray.900"
-                color="white"
-                fontWeight="medium"
-                _hover={{ bg: 'gray.700' }}
-                _active={{
-                  bg: 'gray.800',
-                  transform: 'scale(0.95)'
-                }}
-              >
-                Save
-              </Button>
-            </Box>
           </chakra.form>
         </GridItem>
       </SimpleGrid>
