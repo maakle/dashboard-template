72ad9d58732da890454cd90737284074aefe2614
Mathias Klenk <mathiasklenk@gmx.de>
2022-08-02 19:26:27 +0200

    Added new table for organization invite

--- pages/api/organization/team.ts
+++ pages/api/organization/team.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import NextCors from 'nextjs-cors';
+import prisma from '../../../lib/prisma';
 import { sendOrganizationInvite } from '../../../services/EmailService';
 
 export default async function handler(
@@ -17,10 +18,25 @@ export default async function handler(
       try {
         const { organizationId, email } = req.body;
 
-        // TODO: Create Email invite in template
+        const user = await prisma.user.findUnique({
+          where: {
+            email
+          }
+        });
 
-        sendOrganizationInvite(email);
-        res.status(200).json({ message: 'Email sent' });
+        if (user) {
+          // Add to project
+          res.status(200).json({ message: 'User added to organization' });
+        } else {
+          // Send invite to new user
+          // TODO add to invite table
+          const expiryDate = new Date();
+          expiryDate.setDate(expiryDate.getDate() + 7); // Invite expires after 7 days
+
+
+          sendOrganizationInvite(email);
+          res.status(200).json({ message: 'Email sent' });
+        }
       } catch (error) {
         res.status(500).json({
           error: 'Error occurred while retrieving your organization.'
