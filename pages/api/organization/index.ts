8d9c734fbe3d32607b93c1c548a55500f79892f0
Mathias Klenk <mathiasklenk@gmx.de>
2022-08-02 14:22:55 +0200

    refactored into switch case

--- pages/api/organization/index.ts
+++ pages/api/organization/index.ts
@@ -9,42 +9,46 @@ export default async function handler(
   res: NextApiResponse
 ) {
   await NextCors(req, res, {
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    methods: ['GET', 'PATCH'],
     origin: '*',
     optionsSuccessStatus: 200
   });
 
-  if (req.method === 'GET') {
-    try {
-      const session = await unstable_getServerSession(req, res, authOptions);
-      // TODO: Implement feature for active organization. Now just pick the top
-      const membership = session.user.memberships[0];
-      const organization = await prisma.organization.findUnique({
-        where: { id: membership.organizationId }
-      });
+  switch (req.method) {
+    case 'GET': {
+      try {
+        const session = await unstable_getServerSession(req, res, authOptions);
+        // TODO: Implement feature for active organization. Now just pick the top
+        const membership = session.user.memberships[0];
+        const organization = await prisma.organization.findUnique({
+          where: { id: membership.organizationId }
+        });
 
-      res.status(200).json(organization);
-    } catch (error) {
-      res
-        .status(500)
-        .json({ err: 'Error occurred while retrieving your organization.' });
+        res.status(200).json(organization);
+      } catch (error) {
+        res.status(500).json({
+          error: 'Error occurred while retrieving your organization.'
+        });
+      }
+      break;
     }
-  }
 
-  if (req.method === 'PATCH') {
-    try {
-      const { organizationId, name } = req.body;
-      const newOrganization = await prisma.organization.update({
-        where: { id: organizationId },
-        data: {
-          name: name
-        }
-      });
-      res.status(200).json(newOrganization);
-    } catch (error) {
-      res
-        .status(500)
-        .json({ err: 'Error occurred while updating your organization.' });
+    case 'PATCH': {
+      try {
+        const { organizationId, name } = req.body;
+        const newOrganization = await prisma.organization.update({
+          where: { id: organizationId },
+          data: {
+            name: name
+          }
+        });
+        res.status(200).json(newOrganization);
+      } catch (error) {
+        res
+          .status(500)
+          .json({ error: 'Error occurred while updating your organization.' });
+      }
+      break;
     }
   }
 }
